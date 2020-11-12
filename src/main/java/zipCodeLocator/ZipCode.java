0b911dfd3a12aed36f56ffcd23d09a1d016255b5
NotSoFearfulSymmetry <sapowersschaub@madisoncollege.edu>
2020-11-11 18:55:56 -0600

    created ZipCode.java class

--- src/main/java/zipCodeLocator/ZipCode.java
+++ src/main/java/zipCodeLocator/ZipCode.java
@@ -1,1 +1,69 @@
-package
+package zipCodeLocator;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class ZipCode {
+    @JsonProperty("zip")
+    private int zip;
+
+    @JsonProperty("distance")
+    private double distance;
+
+    @JsonProperty("city")
+    private String city;
+
+    @JsonProperty("state")
+    private String state;
+
+    public ZipCode() {
+    }
+
+    public ZipCode(int zip, double distance, String city, String state) {
+        this.zip = zip;
+        this.distance = distance;
+        this.city = city;
+        this.state = state;
+    }
+
+    public int getZip() {
+        return zip;
+    }
+
+    public void setZip(int zip) {
+        this.zip = zip;
+    }
+
+    public double getDistance() {
+        return distance;
+    }
+
+    public void setDistance(double distance) {
+        this.distance = distance;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    @Override
+    public String toString() {
+        return "ZipCode{" +
+                "zip=" + zip +
+                ", distance=" + distance +
+                ", city='" + city + '\'' +
+                ", state='" + state + '\'' +
+                '}';
+    }
+}
